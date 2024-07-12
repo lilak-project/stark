b04d81659a12c62c8116559c458782da44703f1a
Jung Woo Lee <phyjics@gmail.com>
2024-07-12 18:37:44 +0900

    run_eve.C : AddEveTask() is removed, config_conv.mac for searching mfm files

--- macros/run_eve.C
+++ macros/run_eve.C
@@ -3,9 +3,9 @@ void run_eve()
     auto run = new LKRun();
     run -> AddDetector(new STARK());
     run -> AddPar("config_eve.mac");
-    run -> AddEveTask(new SKSetSiChannelTask);
-    run -> AddEveTask(new SKDrawEventStatisticsTask);
-    run -> AddEveTask(new SKDrawWaveformTask);
+    run -> Add(new SKSetSiChannelTask());
+    run -> Add(new SKDrawEventStatisticsTask);
+    run -> Add(new SKDrawWaveformTask);
     run -> Add(new LKEveTask);
     run -> Init();
     run -> ExecuteNextEvent();
